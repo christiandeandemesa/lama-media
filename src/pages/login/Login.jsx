b7276ef116cb6516470cbf4e9defa5ba2340887c
Christian Dean <christiandeandemesa@gmail.com>
2023-02-17 18:33:31 -0800

    Login design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,36 @@
+// LOGIN PAGE
 import "./Login.scss";
 
 function Login() {
-  return <div className="login">Login</div>;
+  return (
+    // CONTAINER
+    <div className="login">
+      {/* LOGIN CARD */}
+      <div className="card">
+        {/* LEFT SIDE OF CARD */}
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
+            beatae repellat dolor sequi odio voluptate qui sed placeat vel
+            delectus?
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+
+        {/* RIGHT SIDE OF CARD */}
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
