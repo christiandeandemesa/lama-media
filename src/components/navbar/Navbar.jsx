db33b9b4b3360504e9c904fd9bb68726c8a976a9
Christian Dean <christiandeandemesa@gmail.com>
2023-02-17 23:28:19 -0800

    Navbar frontend

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,57 @@
+// NAVBAR
+import { Link } from "react-router-dom";
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 import "./Navbar.scss";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div class="navbar">
+      {/* LEFT SIDE OF NAVBAR */}
+      <div className="left">
+        {/* LOGO */}
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>lamasocial</span>
+        </Link>
+
+        {/* LEFT ICONS */}
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+
+        {/* SEARCH INPUT */}
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+
+      {/* RIGHT SIDE OF NAVBAR */}
+      <div className="right">
+        {/* RIGHT ICONS */}
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+
+        {/* USER AVATAR */}
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
